c7314b95399b5719ba185aca66f168c4acd0439e
lam <yeshusuper@qq.com>
2016-10-26 23:17:50 +0800

    增加文件操作基础类

---
+++ Flh/IO/FileId.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flh.IO
+{
+    public class FileId
+    {
+        public string Id { get; private set; }
+
+        private FileId(string id)
+        {
+            this.Id = id;
+        }
+
+        public static FileId FromFileName(string filename)
+        {
+            ExceptionHelper.ThrowIfNullOrWhiteSpace(filename, "filename");
+            var guid = Guid.NewGuid().ToString("N");
+            var extension = Path.GetExtension(filename);
+            if (!String.IsNullOrWhiteSpace(extension))
+                guid += extension;
+            return new FileId(guid);
+        }
+
+        public static FileId FromFileId(string fileId)
+        {
+            ExceptionHelper.ThrowIfNullOrWhiteSpace(fileId, "fileId");
+            return new FileId(fileId);
+        }
+
+        internal string ToPath(string root)
+        {
+            var arr = new string[4];
+            arr[0] = Id.Substring(0, 1);
+            arr[1] = Id.Substring(1, 1);
+            arr[2] = Id.Substring(2, 1);
+            arr[3] = Id;
+            return Path.Combine(root, String.Join("/", arr));
+        }
+
+        public override string ToString()
+        {
+            return Id;
+        }
+    }
+}
---
+++ Flh/IO/FileManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flh.IO
+{
+    public class FileManager
+    {
+        private IFileStore _FileStore;
+
+        public void Copy(FileId sourceId, FileId destId)
+        {
+            ExceptionHelper.ThrowIfNull(sourceId, "sourceId");
+            ExceptionHelper.ThrowIfNull(destId, "destId");
+            _FileStore.Copy(sourceId, destId);
+        }
+
+        public void CreateOrUpdate(FileId id, Stream stream)
+        {
+            ExceptionHelper.ThrowIfNull(id, "id");
+            ExceptionHelper.ThrowIfNull(stream, "stream");
+            _FileStore.CreateOrUpdate(id, stream);
+        }
+
+        public void CreateOrAppend(FileId id, Stream stream)
+        {
+            ExceptionHelper.ThrowIfNull(id, "id");
+            ExceptionHelper.ThrowIfNull(stream, "stream");
+            _FileStore.CreateOrAppend(id, stream);
+        }
+
+        public void Delete(FileId id)
+        {
+            ExceptionHelper.ThrowIfNull(id, "id");
+            _FileStore.Delete(id);
+        }
+
+        public bool Exists(FileId id)
+        {
+            ExceptionHelper.ThrowIfNull(id, "id");
+            return _FileStore.Exists(id);
+        }
+    }
+}
