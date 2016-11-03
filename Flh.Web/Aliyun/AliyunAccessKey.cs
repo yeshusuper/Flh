6d82449ab1d35619c11956c9cee7e49610d98bc0
lam <yeshusuper@qq.com>
2016-11-03 22:08:44 +0800

    增加阿里云密钥读取

---
+++ Flh.Web/Aliyun/AliyunAccessKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flh.Web.Aliyun
+{
+    public class AliyunAccessKey
+    {
+        public string AccessKeyId { get; private set; }
+        public string AccessKeySecret { get; private set; }
+
+        public AliyunAccessKey(string id, string secret)
+        {
+            this.AccessKeyId = id;
+            this.AccessKeySecret = secret;
+        }
+    }
+}
---
+++ Flh.Web/Aliyun/AliyunHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flh.Web.Aliyun
+{
+    public class AliyunHelper
+    {
+        public readonly static AliyunAccessKey AliyunAccessKey;
+
+        static AliyunHelper() {
+            var value = new System.Configuration.AppSettingsReader().GetValue("aliyunAccessKey", typeof(String)) as string;
+            if (value != null)
+            {
+                var arr = value.Split(new []{",", ";"}, StringSplitOptions.RemoveEmptyEntries);
+                if (arr.Length > 1)
+                {
+                    string id = arr[0], secret = arr[1];
+                    if (!String.IsNullOrWhiteSpace(id) && !String.IsNullOrWhiteSpace(secret))
+                    {
+                        AliyunAccessKey = new AliyunAccessKey(id.Trim(), secret.Trim());
+                    }
+                }
+            }
+        }
+    }
+}
