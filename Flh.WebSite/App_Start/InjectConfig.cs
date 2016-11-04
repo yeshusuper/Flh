3071eaa235eb94ecfd635761f585af71416c6f23
lam <yeshusuper@qq.com>
2016-11-04 23:39:02 +0800

    vs抽筋替换掉的东西

--- Flh.WebSite/App_Start/InjectConfig.cs
+++ Flh.WebSite/App_Start/InjectConfig.cs
@@ -1,4 +1,5 @@
 ﻿using Flh.Web;
+using Flh.Web.Aliyun;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,23 @@ namespace Flh.WebSite
 {
     public class InjectConfig
     {
+        private class WebModule : Ninject.Modules.NinjectModule
+        {
+            public override void Load()
+            {
+                Bind<Flh.IO.FileManager>().ToSelf();
+                if (AliyunHelper.AliyunAccessKey == null)
+                    Bind<Flh.IO.IFileStore>().To<Flh.IO.SystemFileStroe>();
+                else
+                    Bind<Flh.IO.IFileStore>().ToConstant(new OSSService("flh-images"));
+            }
+        }
         public static void Register()
         {
             DependencyResolver.SetResolver(new NinjectDependencyResolver(
                 new Flh.Business.Inject.DataModule(),
-                new Flh.Business.Inject.ServiceModule()));
+                new Flh.Business.Inject.ServiceModule(),
+                new WebModule()));
         }
     }
 }
