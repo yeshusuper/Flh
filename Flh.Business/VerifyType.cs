a845b35912fd17c1a4903a34b57b22c58b2c7f2d
Bias Wang <1598842213@qq.com>
2016-12-14 22:45:55 +0800

    添加合并进来的文件

---
+++ Flh.Business/VerifyType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flh.Business.Mobile
+{
+    public enum VerifyType : byte
+    {
+        Common = 1,
+        FormatPwd = 2,
+        Register = 3,
+    }
+}
